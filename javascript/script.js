e8fec9ff544b65427181798122f7150aba90e785
Manoel Rocha <114105012+ManoelRochaS@users.noreply.github.com>
2023-01-03 20:45:15 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,29 @@
 
-let Header = document.getElementById("Header");
-let Interface = document.getElementById("Interface");
-let InterfaceOne = document.getElementById("InterfaceOne");
-let InterfaceTwo = document.getElementById("InterfaceTwo");
-let Title = document.getElementById("Title");
+const header = document.querySelector("header");
+const title = document.getElementById("Title");
+const displayMenu = document.getElementById("displayMenu");
+const displayClick = document.getElementById("displayClick");
+const displayExtensive = document.getElementById("displayExtensive");
+let control = true;
 
-
-function Clique(){
-    Interface.style.display = "none";
-    InterfaceOne.style.display = "flex";
-    Header.style.padding = "0vh 0vh";
-}
-
-function Extensivo(){
-    Interface.style.display = "none";
-    InterfaceTwo.style.display = "flex";
-    Header.style.padding = "0vh 0vh";
+/*Área de configuração do display */
+function Mode(value){
+    displayMenu.style.display = "none";
+    (value == "Click") ? displayClick.style.display = "flex" : displayExtensive.style.display = "flex";
+    header.style.padding = "0vh";
 }
 
 function Return(){
-    Interface.style.display = "flex";
-    InterfaceOne.style.display = "none";
-    InterfaceTwo.style.display = "none";
-    Header.style.padding = "10vh 0vh";
+    displayMenu.style.display = "flex";
+    displayClick.style.display = "none";
+    displayExtensive.style.display = "none";
+    header.style.padding = "10vh 0vh";
 }
 
-let control = true;
+/*Área de configuração das cores e mudanças necessárias */
 function Action(){
-    if(control){
-        Title.innerText = "Modo Noturno";
-        document.body.classList.add('Black');
-        control = false;
-    }
-    else{
-        Title.innerText = "Modo Diário";
-        document.body.classList.remove('Black');
-        control = true;
-    }
+    (control) ? title.innerText = "Modo Noturno" : title.innerText = "Modo Diário";
+    (control) ? document.body.classList.add('bodyDark') : document.body.classList.remove('bodyDark');
+    (control) ? control = false : control = true;
 }
+
