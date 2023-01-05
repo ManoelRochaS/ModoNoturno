385ffca40f42bd2c05c12e278d91053e00e77396
Manoel Rocha <114105012+ManoelRochaS@users.noreply.github.com>
2023-01-05 14:58:51 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,15 @@
 
+/*Declaração de Constantes (Id e Class) - header, title, displayMenu, displayClick e displayExtensive*/
 const header = document.querySelector("header");
 const title = document.getElementById("Title");
 const displayMenu = document.getElementById("displayMenu");
 const displayClick = document.getElementById("displayClick");
 const displayExtensive = document.getElementById("displayExtensive");
+
+/*Declaração de Variáveis (Controle) - control*/
 let control = true;
 
-/*Área de configuração do display */
+/*Funções Principais - Mode(value), Return() e Engine()*/
 function Mode(value){
     displayMenu.style.display = "none";
     (value == "Click") ? displayClick.style.display = "flex" : displayExtensive.style.display = "flex";
@@ -20,8 +23,7 @@ function Return(){
     header.style.padding = "10vh 0vh";
 }
 
-/*Área de configuração das cores e mudanças necessárias */
-function Action(){
+function Engine(){
     (control) ? title.innerText = "Modo Noturno" : title.innerText = "Modo Diário";
     (control) ? document.body.classList.add('bodyDark') : document.body.classList.remove('bodyDark');
     (control) ? control = false : control = true;
